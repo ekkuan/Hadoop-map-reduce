ba2d502244c3d27bd65c19fcfe532ebff9321ab4
ekkuan <erickkuan@gmail.com>
2017-02-28 22:13:07 -0800

    init

---
+++ src/main/java/org/apache/maven/maven/ReduceClass.java
@@ -0,0 +1,25 @@
+package org.apache.maven.maven;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ReduceClass extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+	protected void reduce(Text key, Iterable<IntWritable> values,
+			Reducer<Text, IntWritable, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+	
+		int sum = 0;
+		Iterator<IntWritable> valuesIt = values.iterator();
+		
+		while(valuesIt.hasNext()){
+			sum = sum + ((IntWritable) valuesIt.next()).get();
+		}
+		
+		context.write(key, new IntWritable(sum));
+	}	
+}
